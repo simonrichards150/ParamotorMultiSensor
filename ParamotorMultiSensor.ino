2b05765c186269b562f42ada1e90f406e0a11299
simonrichards150 <simonrichards150@gmail.com>
2024-03-30 01:16:07 +0000

    Revert "Add LED handler"
    This reverts commit 6e8389525746252703de77e6b4dc21f0c7381ea8.

--- ParamotorMultiSensor.ino
+++ ParamotorMultiSensor.ino
@@ -5,7 +5,6 @@
 #include "include/Temperature.h"
 #include "include/Tach.h"
 #include "include/MicroSD.h"
-#include "include/LEDs.h"
 #include "OneButton.h"
 
 PowerHandler PWR = PowerHandler();
@@ -14,7 +13,8 @@ GPSHandler GPS = GPSHandler();
 TempHandler TEMP = TempHandler();
 TachHandler TACH = TachHandler();
 MicroSDHandler SD = MicroSDHandler();
-LEDHandler LEDS = LEDHandler();
+
+
 
 
 void setup() {
@@ -23,7 +23,6 @@ void setup() {
   GUI.begin();
   GUI.loadMainView();
   commsSetup();
-  LEDS.begin();
   TEMP.begin(); 
   TACH.begin();
   SD.begin();
@@ -50,14 +49,12 @@ void loop() {
   //Get new GPS position etc
   GPS.tick(0); 
 
-  //Refresh the GUI
+  //Update the GUI
   GUI.update(TEMP.getCompTemp(),TACH.getRPM(), GPS.fix, GPS.sats.toInt(), GPS.epochTime(), GPS.hdg.toInt(), PWR.getBatteryMillivolts(), PWR.isCharging(), SD.getStatus()); 
 
   //Update MicroSD handler with new values
   SD.tick(GPS.epochTime(), GPS.fix, GPS.lat, GPS.latNS, GPS.lon, GPS.lonEW, GPS.hdg.toInt(), GPS.spd.toInt(), GPS.alt.toInt(), GPS.sats.toInt(), GPS.hdop.toDouble(), GPS.vdop.toDouble(), GPS.pdop.toDouble(), TACH.getRPM(), TEMP.getCompTemp(), PWR.getBatteryMillivolts());
 
-  //Refresh the LEDs
-  LEDS.tick(TEMP.getCompTemp());
 
   //delay(500); 
 }
